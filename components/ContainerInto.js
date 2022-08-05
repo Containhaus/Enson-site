2644306e8b11a5802d9a103167c2c71f5040f2a4
ContainHaus <containhaus@192.168.1.103>
2022-08-05 13:38:14 +0300

    test

--- components/ContainerInto.js
+++ components/ContainerInto.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import windowSize from "../utils/windowSize";
+import Image from "next/image";
 
 const ContainerInto = ({ data,className }) => {
   const [width] = windowSize();
@@ -97,7 +98,12 @@ const textColorContent = ()=>{
   return (
   
   <>
-     <div className="intro-img" style={{ backgroundImage: `url(${test()})` }}>
+     <div className="intro-img">
+      <Image
+
+        src={test()}
+        layout="fill"
+      />
  
        <div className="intro-content">
         <h1 className={`${textColor()} fw-600`}>{data.title.second}</h1>
