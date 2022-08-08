5429b11225d91e55eef390ba33428907f023f1d3
ContainHaus <containhaus@192.168.1.103>
2022-08-08 14:48:21 +0300

    fsljf

--- components/ContainerModal.js
+++ components/ContainerModal.js
@@ -10,7 +10,7 @@ const ContainerModal = ({ data }) => {
         className="banner-img vh-100"
         style={{ backgroundImage: `url(${data.image})` }}
       >
-        <div className="banner-content col-lg-4 col-md-4 col-sm-6 col-12">
+        <div className="banner-content col-lg-4 col-md-4  col-sm-6 col-12">
           <h5>{data.title.first}</h5>
           <h5>{data.title.second}</h5>
           <p>{data.content.first}</p>
