33a2f6a2c413ab04a7e5bc3d5b4152f34acd02c1
ContainHaus <containhaus@hilal.local>
2022-08-14 16:09:23 +0300

    jdlsfl

--- components/Header.js
+++ components/Header.js
@@ -114,8 +114,8 @@ const Header = () => {
                   </li>
                 
                   <li className="nav-item">
-                    <Link href="/tiny-house">
-                      <a className="nav-link" onClick={handleClick}>Tiny House</a>
+                    <Link href="/special-edition">
+                      <a className="nav-link" onClick={handleClick}>Special Edition</a>
                     </Link>
                   </li>
                   <li className="nav-item">
