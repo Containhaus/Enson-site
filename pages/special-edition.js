0d61dffe28d09bbcf639946ba8f152411143fd69
ContainHaus <containhaus@hilal.local>
2022-08-24 23:02:19 +0300

    lkjflsj

--- pages/special-edition.js
+++ pages/special-edition.js
@@ -28,7 +28,7 @@ const Burocontaıner = ({specialeditatons}) => {
     <div>
          <Head>
         <title>Contain Haus | Special Edition Container </title>
-        <meta name="viewport " content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport " content="initial-scale=1.0,  width=device-width" />
       </Head>
         <ContainerInto data={specialeditatons}></ContainerInto>
         <ContainerDetail></ContainerDetail>
