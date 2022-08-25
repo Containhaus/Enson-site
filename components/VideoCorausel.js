495bc1ff9ca1f8ff2afccdb8271c9f7f978c103e
ContainHaus <containhaus@192.168.1.107>
2022-08-25 10:59:18 +0300

    lkfjsjls

--- components/VideoCorausel.js
+++ components/VideoCorausel.js
@@ -57,7 +57,7 @@ export default function VideoCorausel() {
           <ModalVideo
             autoplay
             isOpen={isOpen1}
-            videoId="3JSit_voEqc"
+            videoId="As8XIPb9-NA"
             onClose={() => setOpen1(false)}
           />
         )}
@@ -65,7 +65,7 @@ export default function VideoCorausel() {
           <ModalVideo
             autoplay
             isOpen={isOpen2}
-            videoId="hJjpfnFyv74"
+            videoId="3JSit_voEqc"
             onClose={() => setOpen2(false)}
           />
         )}
@@ -81,7 +81,7 @@ export default function VideoCorausel() {
           <ModalVideo
             autoplay
             isOpen={isOpen4}
-            videoId="As8XIPb9-NA"
+            videoId="xHqGD4TSAtM"
 
             onClose={() => setOpen4(false)}
           />
@@ -90,7 +90,7 @@ export default function VideoCorausel() {
           <ModalVideo
             autoplay
             isOpen={isOpen5}
-            videoId="xHqGD4TSAtM"
+            videoId="a8FQaW9XVlU"
             onClose={() => setOpen5(false)}
           />
         )}
@@ -98,7 +98,7 @@ export default function VideoCorausel() {
           <ModalVideo
             autoplay
             isOpen={isOpen6}
-            videoId="a8FQaW9XVlU"
+            videoId="jDHpSpilFuU"
             onClose={() => setOpen6(false)}
           />
         )}
@@ -106,7 +106,7 @@ export default function VideoCorausel() {
           <ModalVideo
             autoplay
             isOpen={isOpen7}
-            videoId="jDHpSpilFuU"
+            videoId="hJjpfnFyv74"
             onClose={() => setOpen7(false)}
           />
         )}
@@ -114,7 +114,15 @@ export default function VideoCorausel() {
           <ModalVideo
             autoplay
             isOpen={isOpen8}
-            videoId="hJjpfnFyv74"
+            videoId="YGVXtLyGD5Q"
+            onClose={() => setOpen8(false)}
+          />
+        )}
+        {typeof window !== "undefined" && (
+          <ModalVideo
+            autoplay
+            isOpen={isOpen8}
+            videoId="W_kfISQx0pM"
             onClose={() => setOpen8(false)}
           />
         )}
@@ -122,18 +130,12 @@ export default function VideoCorausel() {
           <ModalVideo
             autoplay
             isOpen={isOpen9}
-            videoId="W_kfISQx0pM"
+            videoId="YGVXtLyGD5Q"
             onClose={() => setOpen9(false)}
           />
         )}
-        {typeof window !== "undefined" && (
-          <ModalVideo
-            autoplay
-            isOpen={isOpen10}
-            videoId="YGVXtLyGD5Q"
-            onClose={() => setOpen10(false)}
-          />
-        )}
+        
+       
         <div className="wrapper pt-120">
           <Splide
             classes={{
@@ -170,13 +172,13 @@ export default function VideoCorausel() {
                   >
                     <Image
                       className="VideoDes"
-                      src="/assets/img/Youtubekapak2/1.jpg"
+                      src="/assets/img/Youtubekapak2/1.webp"
                       layout="fill"
                       quality={70}
                     />
                     <Image
                       className="VideoMob"
-                      src="/assets/img/Youtubekapak2/Mobil/2.webp"
+                      src="/assets/img/Youtubekapak2/Mobil/1.webp"
                       layout="fill"
                       quality={70}
                     />
@@ -188,7 +190,7 @@ export default function VideoCorausel() {
                           e.preventDefault();
                           setOpen1(true);
                         }}
-                        href="https://youtu.be/3JSit_voEqc"
+                        href="https://youtu.be/As8XIPb9-NA"
                       >
                         <div className="vid-butn">
                           <span className="icon">
@@ -214,13 +216,13 @@ export default function VideoCorausel() {
                   >
                     <Image
                       className="VideoDes"
-                      src="/assets/img/Youtubekapak2/2.jpg"
+                      src="/assets/img/Youtubekapak2/2.webp"
                       layout="fill"
                       quality={70}
                     />
                     <Image
                       className="VideoMob"
-                      src="/assets/img/Youtubekapak2/Mobil/4.webp"
+                      src="/assets/img/Youtubekapak2/Mobil/2.webp"
                       layout="fill"
                       quality={70}
                     />
@@ -232,7 +234,7 @@ export default function VideoCorausel() {
                           e.preventDefault();
                           setOpen2(true);
                         }}
-                        href="https://youtu.be/hJjpfnFyv74"
+                        href="https://youtu.be/3JSit_voEqc"
                       >
                         <div className="vid-butn">
                           <span className="icon">
@@ -257,7 +259,7 @@ export default function VideoCorausel() {
                   >
                     <Image
                       className="VideoDes"
-                      src="/assets/img/Youtubekapak2/3.jpg"
+                      src="/assets/img/Youtubekapak2/3.webp"
                       layout="fill"
                       quality={70}
                     />
@@ -304,7 +306,7 @@ export default function VideoCorausel() {
                     />
                     <Image
                       className="VideoMob"
-                      src="/assets/img/Youtubekapak2/Mobil/1.webp"
+                      src="/assets/img/Youtubekapak2/Mobil/4.webp"
                       layout="fill"
                       quality={70}
                     />
@@ -316,49 +318,6 @@ export default function VideoCorausel() {
                           showModal4
                           e.preventDefault();
                           setOpen4(true);
-                        }}
-                        href="https://youtu.be/As8XIPb9-NA"
-                      >
-                        <div className="vid-butn">
-                          <span className="icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
-                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
-                            </svg>
-                          </span>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </SplideSlide>
-              <SplideSlide >
-                <div className="container-fluid pl-0 pr-0">
-                  <div
-                    className="video-wrapper section-padding bg-img parallaxie valign"
-
-                    data-overlay-dark="4"
-                  >
-                    <Image
-                      className="VideoDes"
-                      src="/assets/img/Youtubekapak2/5.jpg"
-                      layout="fill"
-                      quality={70}
-                    />
-                    <Image
-                      className="VideoMob"
-                      src="/assets/img/Youtubekapak2/Mobil/5.webp"
-                      layout="fill"
-                      quality={70}
-                    />
-                    <div className="full-width text-center">
-                      <a
-                        className="vid"
-
-                        onClick={(e) => {
-                          showModal5
-                          e.preventDefault();
-                          setOpen5(true);
                         }}
                         href="https://youtu.be/xHqGD4TSAtM"
                       >
@@ -384,13 +343,56 @@ export default function VideoCorausel() {
                   >
                     <Image
                       className="VideoDes"
-                      src="/assets/img/Youtubekapak2/6.jpg"
+                      src="/assets/img/Youtubekapak2/5.webp"
                       layout="fill"
                       quality={70}
                     />
                     <Image
                       className="VideoMob"
-                      src="/assets/img/Youtubekapak2/Mobil/2.webp"
+                      src="/assets/img/Youtubekapak2/Mobil/5.webp"
+                      layout="fill"
+                      quality={70}
+                    />
+                    <div className="full-width text-center">
+                      <a
+                        className="vid"
+
+                        onClick={(e) => {
+                          showModal5
+                          e.preventDefault();
+                          setOpen5(true);
+                        }}
+                        href="https://youtu.be/a8FQaW9XVlU"
+                      >
+                        <div className="vid-butn">
+                          <span className="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
+                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                            </svg>
+                          </span>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </SplideSlide>
+              <SplideSlide >
+                <div className="container-fluid pl-0 pr-0">
+                  <div
+                    className="video-wrapper section-padding bg-img parallaxie valign"
+
+                    data-overlay-dark="4"
+                  >
+                    <Image
+                      className="VideoDes"
+                      src="/assets/img/Youtubekapak2/6.webp"
+                      layout="fill"
+                      quality={70}
+                    />
+                    <Image
+                      className="VideoMob"
+                      src="/assets/img/Youtubekapak2/Mobil/6.webp"
                       layout="fill"
                       quality={70}
                     />
@@ -404,7 +406,7 @@ export default function VideoCorausel() {
                           e.preventDefault();
                           setOpen6(true);
                         }}
-                        href="https://youtu.be/a8FQaW9XVlU"
+                        href="https://youtu.be/jDHpSpilFuU"
                       >
                         <div className="vid-butn">
                           <span className="icon">
@@ -435,7 +437,7 @@ export default function VideoCorausel() {
                     />
                     <Image
                       className="VideoMob"
-                      src="/assets/img/Youtubekapak2/Mobil/6.webp"
+                      src="/assets/img/Youtubekapak2/Mobil/7.webp"
                       layout="fill"
                       quality={70}
                     />
@@ -449,7 +451,7 @@ export default function VideoCorausel() {
                           e.preventDefault();
                           setOpen7(true);
                         }}
-                        href="https://youtu.be/jDHpSpilFuU"
+                        href="https://youtu.be/hJjpfnFyv74"
                       >
                         <div className="vid-butn">
                           <span className="icon">
@@ -473,13 +475,13 @@ export default function VideoCorausel() {
                     data-overlay-dark="4"
                   >
                     <Image
-                      src="/assets/img/Youtubekapak2/8.jpg"
+                      src="/assets/img/Youtubekapak2/8.webp"
                       layout="fill"
                       quality={70}
                     />
                     <Image
                       className="VideoMob"
-                      src="/assets/img/Youtubekapak2/Mobil/8.jpg"
+                      src="/assets/img/Youtubekapak2/Mobil/8.webp"
                       layout="fill"
                       quality={70}
                     />
@@ -493,7 +495,7 @@ export default function VideoCorausel() {
                           e.preventDefault();
                           setOpen8(true);
                         }}
-                        href="https://youtu.be/hJjpfnFyv74"
+                        href="https://youtu.be/W_kfISQx0pM"
                       >
                         <div className="vid-butn">
                           <span className="icon">
@@ -537,50 +539,6 @@ export default function VideoCorausel() {
                           e.preventDefault();
                           setOpen9(true);
                         }}
-                        href="https://youtu.be/W_kfISQx0pM"
-                      >
-                        <div className="vid-butn">
-                          <span className="icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
-                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
-                            </svg>
-                          </span>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-              </SplideSlide>
-              <SplideSlide >
-                <div className="container-fluid pl-0 pr-0">
-                  <div
-                    className="video-wrapper section-padding bg-img parallaxie valign"
-
-                    data-overlay-dark="4"
-                  >
-                    <Image
-                      src="/assets/img/Youtubekapak2/10.jpg"
-                      layout="fill"
-                      quality={70}
-                    />
-                    <Image
-                      className="VideoMob"
-                      src="/assets/img/Youtubekapak2/Mobil/10.jpg"
-                      layout="fill"
-                      quality={70}
-                    />
-                    <div className="full-width text-center">
-
-                      <a
-                        className="vid"
-
-                        onClick={(e) => {
-                          showModal10
-                          e.preventDefault();
-                          setOpen10(true);
-                        }}
                         href="https://youtu.be/YGVXtLyGD5Q"
                       >
                         <div className="vid-butn">
@@ -597,6 +555,7 @@ export default function VideoCorausel() {
                 </div>
 
               </SplideSlide>
+              
               
             
               
