63f3528fee10c78619e6d0aaafab7ea1cf0750be
ContainHaus <containhaus@192.168.1.107>
2022-08-31 15:54:52 +0300

    jfslk

--- components/BuroContainerIntro.js
+++ components/BuroContainerIntro.js
@@ -10,11 +10,12 @@ const BuroContainerIntro = ({ data }) => {
       <div className="intro-img" >
 
         <Image
-          layout="fill"
+          width={1920}
+          height={1080}
           objectFit="cover"
           objectPosition=" center"
           src={width > 768 ? data.image1 : data.mobileImage1}
-          priority
+          
         />
 
    
